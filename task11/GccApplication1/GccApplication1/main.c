a6ee7f2576101461e6eda0ef10db20cddb5aa6e8
Paul Chen <pualchenhr@hotmail.com>
2019-11-28 11:52:05 -0800

    task11

--- task11/GccApplication1/GccApplication1/main.c
+++ task11/GccApplication1/GccApplication1/main.c
@@ -6,31 +6,35 @@
  */ 
 
 #include <avr/io.h>
-//#include <until/delay.h>
+#include <util/delay.h>
 #include <avr/interrupt.h>
 int setup(void)
 {
-	DDRB |=(1<<PB3) | (1<<PB5) | (1<<PB6);	
+	DDRB |=(1<<PB3) | (1<<PB5) | (1<<PB6)|(1<<PB2);	
 	SPCR |= (1<<SPE)|(1<<CPOL)|(1<<MSTR);
+	PORTB |= (1<<PB2);//SS high
 }
 
 int main(void)
 {
 	setup();
-    uint8_t data;
-	uint8_t datareceieved;
+    //uint8_t data;
+	uint8_t datareceived;
 	int i;
 	while (1)
 	{
+		
 		for(i=0;i<255;i++)
 		{
-			data = i;
+			
+			
 			PORTB &= ~(1<<PB6);//pulls chip select low
-			SPDR = data;
+			SPDR = i;
 			while(!(SPSR & (1<<SPIF)));
-			datareceieved = SPDR;
+			
+			datareceived = SPDR;
 			PORTB |= (1<<PB6);//set chip select high	
-			_delay_ms(200);
+			_delay_ms(100);
 		}
 	}
 
